04943d70dd7258c5e75aa9ff8e378c7583f1f5b9
Ben Lardie <benlardie@gmail.com>
2019-04-01 13:43:42 -0400

    geting the api using axios

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const catButton = document.querySelector('.summon-cats');
     catButton.addEventListener('click', function() {
+        axios.get('http://bitkittens.herokuapp.com/cats.json')
+        .then((response) => {
+            console.log(response)
+        })
         console.log('you clicked that button you dirty dog!');
     });
 });
