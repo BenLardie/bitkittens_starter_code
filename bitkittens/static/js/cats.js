fb0a0cc3fc5b64fc3efd0d64bb1a233080400668
Ben Lardie <benlardie@gmail.com>
2019-04-01 14:40:07 -0400

    finished

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -3,12 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     catButton.addEventListener('click', function() {
         axios.get('http://bitkittens.herokuapp.com/cats.json')
         .then((response) => {
-        let div1 = document.querySelector('#cat1');
+        let divs = document.querySelectorAll('div');
         let catsList = response.data.cats;
-        let img = document.createElement('img');
-        img.src = catsList[0]['photo']
-        div1.appendChild(img)
-        console.log('you clicked that button you dirty dog!');
+        catsList.forEach( (cat, i=0) => {
+        let img = document.createElement('img'); 
+        img.src = catsList[i]['photo'];
+        img.alt = catsList[i]['name'];
+        divs[i].appendChild(img);
+        i++;
+        });
     });
 })
 });
