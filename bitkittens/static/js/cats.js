ee4edd78cdec104b76e48aeb9610cc8bcfc5e054
Ben Lardie <benlardie@gmail.com>
2019-04-01 13:39:15 -0400

    saved button in var and event listener works

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const catButton = document.querySelector('.summon-cats');
+    catButton.addEventListener('click', function() {
+        console.log('you clicked that button you dirty dog!');
+    });
 });
